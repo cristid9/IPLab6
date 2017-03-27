3747d2bf665cce3f8afe2a251302e0f0798828d8
cristid9 <cristi.dnla@gmail.com>
2017-03-27 14:57:07 +0300

    Added implementation for Shape

--- src/Sides.java
+++ src/Sides.java
@@ -1,3 +1,18 @@
-public class Sides implements ISides {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Sides implements ISides {
+    List<Point> points;
+
+    public Sides() {
+        points = new ArrayList<>();
+    }
+
+    public void addPoint(Point p) {
+        points.add(p);
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
 }
